b14f1951b97610032d8a1981b6ccc764d31c2ac3
Jakub Kubaczynski <kuba808x@interia.pl>
2024-12-08 23:46:30 +0100

    lab6

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,15 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+std::vector<char> foo(std::list<Human>& people)
 {
-    std::vector< char > retval(people.size());
+    std::vector<char> retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
+
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person) {
+        return person.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
